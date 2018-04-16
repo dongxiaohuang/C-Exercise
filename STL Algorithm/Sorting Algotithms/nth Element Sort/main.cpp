be6180bbe92c8eaab61e843b2899e9ae7e54e0e7
dongxiaohuang <dongxiao.huang17@imperial.ac.uk>
2018-04-16 20:10:07 +0100

    add examples of using nth_sort()

---
+++ STL Algorithm/Sorting Algotithms/nth Element Sort/main.cpp
@@ -0,0 +1,50 @@
+#include "helper.h"
+#include <algorithm>
+using namespace std;
+/**
+   void nth_element (RandomAccessIterator beg, RandomAccessIterator nth, RandomAccessIterator end)
+
+   void nth_element (RandomAccessIterator beg, RandomAccessIterator nth, RandomAccessIterator end, BinaryPredicate op)
+ **/
+int main(){
+        deque<int> coll;
+
+        INSERT_ELEMENTS(coll,3,7);
+        INSERT_ELEMENTS(coll,2,6);
+        INSERT_ELEMENTS(coll,1,5);
+        PRINT_ELEMENTS(coll);
+
+// extract the four lowest elements
+        nth_element (coll.begin(),
+                     coll.begin()+3, coll.end());
+
+// beginning of range // element that should be sorted correctly // end of range
+
+// print them
+        cout << "the four lowest elements are: ";
+        copy (coll.cbegin(), coll.cbegin()+4, ostream_iterator<int>(cout," "));
+        cout << endl;
+
+
+// extract the four highest elements
+        nth_element (coll.begin(), coll.end()-4, coll.end());
+
+// beginning of range // element that should be sorted correctly // end of range
+
+// print them
+        cout << "the four highest elements are: ";
+        copy (coll.cend()-4, coll.cend(), ostream_iterator<int>(cout," "));
+        cout << endl;
+
+// extract the four highest elements (second version)
+        nth_element (coll.begin(), // beginning of range
+
+                     coll.begin()+3, // element that should be sorted correctly
+                     coll.end(), // end of range
+                     greater<int>()); // sorting criterion
+
+// print them
+        cout << "the four highest elements are: "; // partially ordered just enough to tell you the nth is correct
+
+        copy (coll.cbegin(), coll.cbegin()+4, ostream_iterator<int>(cout," ")); cout << endl;
+}
